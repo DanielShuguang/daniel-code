1c22487b9eda39ffd4808524a69580e9dba7e370
DanielShuguang <41940314+DanielShuguang@users.noreply.github.com>
2024-03-27 20:22:56 +0800

    chore: 升級依賴，增加 unplugin 插件

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,13 +5,22 @@ import path from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 import monaco from 'vite-plugin-monaco-editor'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // 包的默认导出有问题，不可直接使用
 const monacoEditorPlugin: typeof monaco = (monaco as any).default
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), monacoEditorPlugin({})],
+  plugins: [
+    vue(),
+    vueJsx(),
+    monacoEditorPlugin({}),
+    AutoImport({
+      imports: ['vue', 'vue-router', '@vueuse/core', 'pinia', 'vitest'],
+      dts: 'types/auto-import.d.ts'
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
